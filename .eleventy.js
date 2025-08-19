7f63f6183e2aa284359bd96b3af18491598236f5
Kraaketing <nedogedell@protonmail.com>
2025-08-19 13:05:21 +0100

    camelcase for imported data json files, hyphens are seemingly not being converted by 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -29,6 +29,7 @@ module.exports = function (eleventyConfig) {
     dir: {
       input: "src",
       includes: "_includes",
+      data: "_data",
       output: "_site",
     },
     markdownTemplateEngine: "njk",
